274af1bc99d22d78c46d66a333a2a2e692da709b
Arto Roth <arto.roth@corellia.fi>
2026-03-26 13:37:25 +0200

    Autovisa v2

--- api/fetchCarImages/__init__.py
+++ api/fetchCarImages/__init__.py
@@ -2,12 +2,17 @@ import logging
 import azure.functions as func
 from .image_utils import fetch_and_crop_car_image
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Processing request to fetch and crop car image.")
-    
+    logging.info("Processing request to fetch and validate car image.")
+
     try:
         result = fetch_and_crop_car_image()
         return func.HttpResponse(result, mimetype="application/json")
     except Exception as e:
-        logging.error(f"Error: {e}")
-        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
+        logging.exception("Unhandled error while processing car image request")
+        return func.HttpResponse(
+            f'{{"error": "{str(e)}"}}',
+            mimetype="application/json",
+            status_code=500,
+        )
